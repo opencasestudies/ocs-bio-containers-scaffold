df3f98b2e731893f24abf5a5ed8ef27537c1b8c3
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-07-28 19:27:47 -0400

    add a preprocessing.R script for data extraction within analysis_scripts directory
    Added functionality to extract and save data from a PDF, including handling output directory creation and saving in both RDA and CSV formats.

---
+++ analysis_scripts/preprocessing.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+library(tabulapdf)
+library(here)
+library(readr)
+
+#extract the data
+raw_table <- tabulapdf::extract_tables("https://www.nature.com/articles/s41522-022-00345-5.pdf",
+                                       pages = 2,
+                                       method = "stream",
+                                       col_names = FALSE
+                                      )[[1]]
+
+#output information about the extracted data
+message("Dimensions: ", paste(dim(raw_table), collapse = " x "))
+message("Number of NAs: ", sum(is.na(raw_table)))
+
+#save the data
+output_dir <- here::here("data", "imported")
+
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+
+
+save(raw_table, 
+    file = here::here("data", 
+                      "imported", 
+                      "raw_table1.rda"))
+                      
+readr::write_csv(raw_table, 
+                 file = here::here("data",
+                                   "imported",
+                                   "raw_table1.csv"))

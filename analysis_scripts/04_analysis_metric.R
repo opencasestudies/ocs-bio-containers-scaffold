45cbdf5fc790ea402664afc24d2d7de174c6f8f2
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-09-16 18:07:15 -0400

    Refactor correlation analysis into a reusable function
    Refactored correlation analysis by creating a function to find Spearman correlations for different dataset filters. Removed redundant code for non-longitudinal and adult datasets, streamlining the analysis process.

--- analysis_scripts/04_analysis_metric.R
+++ analysis_scripts/04_analysis_metric.R
@@ -5,45 +5,56 @@ library(tidyverse)
 
 load(here("data", "wrangled", "wrangled_data.rda"))
 
-cor_matrix_all <- table1_extract %>% 
-    select(num_paired_samples, 
-           kegg_annotated_compounds, 
+#' A function to find a correlation matrix given an input data frame
+#'
+#' This function will find the Spearman correlation matrix reporting pairwise Spearman correlations from among the num_paired_samples, kegg_annotated_compounds, and hmdb_annotated_compounds variables
+#' 
+#' @param input_df the input df to find the correlation from
+#'        This may be filtered in a later step to only include datasets that are __ (all adults, all longitudinal, etc.). 
+#'        Needs to have columns num_paired_samples, kegg_annotated_compounds, hmdb_annotated_compounds, cohort_description, and longitudinal.
+#' @param filter_description default is "All Data" assuming that the dataset isn't filtered. 
+#'        Change to reflect how the data should be filtered prior to finding correlations. 
+#'        Options include "Non-longitudinal" and "Adult".
+#'
+#' Will output information rather than returning a variable or object.
+find_cor_matrix <- function(input_df, filter_description = "All Data"){
+  
+  #check input to make sure it is supported
+  stopifnot(filter_description %in% c("All Data", "Non-longitudinal", "Adult"))
+  
+  #filter based off of filter_description option
+  if (filter_description == "Non-longitudinal"){
+    to_assess <- input_df %>%
+      filter(longitudinal == "No")
+  } else if(filter_description == "Adult"){
+    to_assess <- input_df %>%
+      filter(!str_detect(tolower(cohort_description), 
+                         "infants|children"))
+  } else{
+    #Using All Datasets
+    to_assess <- input_df
+  }
+  
+  #Find Spearman correlations
+  cor_matrix <- to_assess %>%
+    select(num_paired_samples,
+           kegg_annotated_compounds,
            hmdb_annotated_compounds) %>%
     cor(., method = "spearman")
+  
+  #output information about the correlation analysis
+  cat("\n", filter_description, "\n")
+  cat("Number of datasets per variable in correlation analysis (", filter_description, ") : ", nrow(to_assess), "\n")
+  cat("Annotated Compounds Spearman Correlation (", filter_description, "): ", cor_matrix["kegg_annotated_compounds", "hmdb_annotated_compounds"], "\n")
+  cat("Number of samples and KEGG Annotated Compounds Spearman Correlation (", filter_description, "): ", cor_matrix["num_paired_samples", "kegg_annotated_compounds"], "\n")
+  cat("HMDB Annotated Compounds and Number of samples Spearman Correlation (", filter_description, "): ", cor_matrix["hmdb_annotated_compounds", "num_paired_samples"], "\n")
+}
 
-#output information about the correlation analysis for all datasets
-cat("All Datasets\n")
-cat("Number of Datasets per variable in correlation analysis (All Data): ", nrow(table1_extract), "\n")
-cat("Annoted Compounds Spearman Correlation (All Data): ", cor_matrix_all["kegg_annotated_compounds", "hmdb_annotated_compounds"], "\n")
-cat("Number of samples and KEGG Annotated Compounds Spearman Corelation (All Data): ", cor_matrix_all["num_paired_samples", "kegg_annotated_compounds"], "\n")
-cat("HMDB Annotated Compounds and Number of samples Spearman Correlation (All Data): ", cor_matrix_all["hmdb_annotated_compounds", "num_paired_samples"], "\n")
+# All datasets
+find_cor_matrix(table1_extract)
 
-cor_matrix_non_longitudinal <- table1_extract %>%
-    filter(longitudinal == "No") %>%
-    select(num_paired_samples, 
-           kegg_annotated_compounds, 
-           hmdb_annotated_compounds) %>%
-    cor(., method = "spearman")
+# Non-longitudinal datasets only
+find_cor_matrix(table1_extract, filter_description = "Non-longitudinal")
 
-#output information about non-longitudinal datasets correlation analysis
-cat("\nNon-longitudinal Datasets\n")
-cat("Number of Datasets per variable in correlation analysis (Non-longitudinal Data): ", nrow(table1_extract %>% filter(longitudinal == "No")), "\n")
-cat("Annoted Compounds Spearman Correlation (Non-longitudinal Data): ", cor_matrix_non_longitudinal["kegg_annotated_compounds", "hmdb_annotated_compounds"], "\n")
-cat("Number of samples and KEGG Annotated Compounds Spearman Corelation (Non-longitudinal Data): ", cor_matrix_non_longitudinal["num_paired_samples", "kegg_annotated_compounds"], "\n")
-cat("HMDB Annotated Compounds and Number of samples Spearman Correlation (Non-longitudinal Data): ", cor_matrix_non_longitudinal["hmdb_annotated_compounds", "num_paired_samples"], "\n")
-
-
-cor_matrix_adult <- table1_extract %>%
-    filter(!str_detect(tolower(cohort_description),
-                      "infants|children")) %>%
-    select(num_paired_samples, 
-           kegg_annotated_compounds, 
-           hmdb_annotated_compounds) %>%
-    cor(., method = "spearman")
-
-#output information about the Adult only datasets correlation analysis
-cat("\nAdult Datasets\n")
-cat("Number of Datasets per variable in correlation analysis (Adult Data): ", nrow(table1_extract %>% filter(!str_detect(tolower(cohort_description), "infants|children"))), "\n")
-cat("Annoted Compounds Spearman Correlation (Adult Data): ", cor_matrix_adult["kegg_annotated_compounds", "hmdb_annotated_compounds"], "\n")
-cat("Number of samples and KEGG Annotated Compounds Spearman Corelation (Adult Data): ", cor_matrix_adult["num_paired_samples", "kegg_annotated_compounds"], "\n")
-cat("HMDB Annotated Compounds and Number of samples Spearman Correlation (Adult Data): ", cor_matrix_adult["hmdb_annotated_compounds", "num_paired_samples"], "\n")
+# Adult datasets only
+find_cor_matrix(table1_extract, filter_description = "Adult")
